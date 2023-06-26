da60a129ce03059dd6acc5069cb7fe0bb09a64b7
rvDev <136956878+RVilums@users.noreply.github.com>
2023-06-26 13:42:24 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./global.styles.scss";
 import Root from "./pages/root.page";
 import Home from "./pages/home.page";
 import Wrapper from "./components/layout/wrapper";
-import Container from "./components/layout/container";
+import Expertise from "./components/expertise/expertise.component";
 
 function App() {
   return (
@@ -11,27 +11,7 @@ function App() {
       <Home />
       <Wrapper>
         <h1>My Expretise</h1>
-        <Container className="container--border">
-          <div className="expertise__heading">
-          <h2>UI/UX Design</h2>
-          {/* Icon Here */}
-          </div>
-          <h3></h3>
-        </Container>
-        <Container className="container--border">
-          <div className="expertise__heading">
-          <h2>Software Development</h2>
-          {/* Icon Here */}
-          </div>
-          <h3></h3>
-        </Container>
-        <Container className="container--border">
-          <div className="expertise__heading">
-          <h2></h2>
-          {/* Icon Here */}
-          </div>
-          <h3></h3>
-        </Container>
+        <Expertise />
       </Wrapper>
     </Root>
   );
